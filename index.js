d4238fd700b9a23c53e3c2bba8fea74bdc219a6e
amanp18 <amanpratap9868@gmail.com>
2025-06-20 01:50:41 +0530

    updated

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import authroutes from "./routes/authroutes.js";
 import recruitroutes from "./routes/Recruitroutes.js"
+import User from './routes/User.js'
+import Employeeroutes from './routes/Employeeroutes.js'
+import message from './routes/message.js'
+import application from './routes/application.js';
 
 const app = express();
 dotenv.config();
@@ -13,6 +17,10 @@ app.get("/", (req, res) => {
 });
 app.use("/api/v4/auth", authroutes);
 app.use("/api/v4/recruiter",recruitroutes)
+app.use('/api/users', User);
+app.use('/api/jobs', Employeeroutes);
+app.use('/api/jobs', application);
+app.use('/api/messages', message);
 connectDB();
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
